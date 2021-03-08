4758a8ec3c53072333944cde3df1eae535bfca2b
Qiling <professorqtaku@gmail.com>
2021-03-08 13:23:55 +0100

    connect to mongoose
    Co-Authored-By: EdvinArnryd <72066252+EdvinArnryd@users.noreply.github.com>
    Co-Authored-By: Axel W <72077876+Gondarc@users.noreply.github.com>
    Co-Authored-By: lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
+global.mongoose = require('mongoose')
 const express = require("express");
 const app = express();
+const path = require('path')
 const { nanoid } = require('nanoid')
 
 app.use(express.json())
 
-app.listen(3001)
+//app.use(express.static(path.join(__dirname, "public")));
+
+const altasUrl =
+  "mongodb+srv://clearbnbAtlas:grupp5@cluster0.p9ure.mongodb.net/clearbnb?retryWrites=true&w=majority";
+
+global.mongoose.connect(altasUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/test', (req, res) => {
+  res.send('In the get')
+})
+
+app.listen(3001, () => {
+  console.log("Server started ar port 3001");
+});
