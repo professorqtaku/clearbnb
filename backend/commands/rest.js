6dd1529a5279bbc7650a28638e98d95f2e27ee77
Qiling <72065263+professorqtaku@users.noreply.github.com>
2021-03-08 20:39:21 +0100

    added app.get for all models
    without populate

--- backend/commands/rest.js
+++ backend/commands/rest.js
@@ -3,9 +3,15 @@ const models = require("../models.js");
 module.exports = (app) => {
 
   app.get('/rest/:model', async (req, res) => {
-    let model = models[req.params.model]
-    console.log(model);
-    res.send('model found')
+    let docs = ''
+    try {
+      docs = await models[req.params.model].find()
+    }
+    catch(e) {
+      res.send('model not found')
+      return
+    }
+    res.json(docs)
   })
 
 };
