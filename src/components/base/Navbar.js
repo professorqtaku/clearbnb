e7d1cb1c162c2e6be6dc496758672f78583dc797
Qiling <professorqtaku@gmail.com>
2021-03-08 16:07:17 +0100

    added brandname & menu to right

--- src/components/base/Navbar.js
+++ src/components/base/Navbar.js
@@ -1,12 +1,9 @@
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from "react-router-dom";
 export default function Navbar() {
-
-  const history = useHistory()
+  const history = useHistory();
 
   function goTo(e) {
-    if(e.target.value) {
-        history.push('/' + e.target.value)
-    }
+    history.push("/" + e.target.value);
   }
 
   return (
@@ -16,28 +13,49 @@ export default function Navbar() {
         style={styles.navbar}
       >
         <div className="container-fluid">
+          <button className="btn navbar-brand me-auto" onClick={goTo} value="">
+            ClearBnB
+          </button>
           <button
             className="navbar-toggler"
             data-bs-toggle="collapse"
             data-bs-target="#navbarContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            id="navbarButton"
           >
             <span className="material-icons" style={styles.icon}>
               menu
             </span>
           </button>
 
-          <div className="collapse navbar-collapse" id="navbarContent" style={ styles.content }>
-            <ul className="navbar-nav">
-              <li className="nav-item nav-link">
-                <button className="btn btn-sm" onClick={goTo} value="register" style={styles.button}>Become member</button>
-              </li>
-              <li className="nav-item nav-link">
-                <button className="btn btn-sm" onClick={goTo} value="login" style={styles.button}>Login</button>
-              </li>
-            </ul>
+          <div className="collapse navbar-collapse" id="navbarContent" style={styles.content}>
+            <div className="mx-auto"></div>
+              <ul className="navbar-nav">
+                <li className="nav-item nav-link">
+                  <button
+                    className="btn btn-sm"
+                    onClick={goTo}
+                    value="register"
+                    style={styles.button}
+                  >
+                    Become member
+                  </button>
+                </li>
+                <li className="nav-item nav-link">
+                  <button
+                    className="btn btn-sm"
+                    onClick={goTo}
+                    value="login"
+                    style={styles.button}
+                  >
+                    Login
+                  </button>
+                </li>
+              </ul>
           </div>
+
+
         </div>
       </nav>
     </div>
@@ -53,8 +71,13 @@ const styles = {
     color: "white",
   },
   button: {
-
+    color: "white",
+    ":focus": {
+      outline: "none !important",
+      boxShadow: "none",
+    },
   },
   content: {
+    transition: "300ms",
   },
 };
