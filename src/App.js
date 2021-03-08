cd3d6a4d6ab6a69db24b654e849c3e73472406ed
Qiling <professorqtaku@gmail.com>
2021-03-08 14:02:32 +0100

    cleaned index

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/HomePage'
 
 function App() {
   return (
     <div className="App">
+        <Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <main>
+          <Switch>
+            <Route exact path="/" component={ Home }/>
+          </Switch>
+      </main>
+
+      <footer>
+
+      </footer>
+      </Router>
     </div>
   );
 }
