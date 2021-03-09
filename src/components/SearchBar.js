e48e1a5df3f8a1dc6b85dd013b91b1d2f3c58554
snabelA00 <ja000411@gmail.com>
2021-03-09 15:34:42 +0100

    searchBar for mobile

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,40 @@
 export default function SearchBar() {
   return (
-    <div style={gridContainer}>
-      <div style={gridItem}>
-        <input placeholder="location"></input>
+    <div style={styles.gridContainer}>
+      <div style={styles.location}>
+        <input style={styles.input} placeholder="location"></input>
       </div>
-      <div style={gridItem}>
-        <input placeholder="Check in"></input>
+      <div style={styles.gridItem}>
+        <input style={styles.input} placeholder="Check in"></input>
       </div>
-      <div style={gridItem}>
-        <input placeholder="Check out"></input>
+      <div style={styles.gridItem}>
+        <input style={styles.input} placeholder="Check out"></input>
       </div>
-      <div style={gridItem}>
-        <input placeholder="Guests"></input>
+      <div style={styles.gridItem}>
+        <input style={styles.input} placeholder="Guests"></input>
       </div>
-      <div style={gridItem}>
-        <button>Search</button>
+      <div style={styles.gridItem}>
+        <button style={styles.input} >Search</button>
       </div>
     </div>
   )
 }
-const gridContainer = {
-  width: '700px',
-  backgroundColor: 'grey',
-  // backgroundColor: 'rgba(255, 255, 255, 0.73)',
-  display: 'grid',
-  gridTemplateRows: '1fr',
-  gridTemplateColumns: 'repeat(5, 1fr)',
-  padding: '20px',
-  
+
+const styles = {
+  gridContainer: {
+    // backgroundColor: 'rgba(255, 255, 255, 0.73)',
+    display: 'grid',
+    gridTemplateRows: '3fr',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    gridGap: '10px'
+  },
+
+  input: {
+    width: '100%'
+  },
+
+  location: {
+    gridArea: '1/1/2/3'
+  }
 }
-const gridItem = {
-  
-}
+
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,24 @@ import SearchBar from "../components/SearchBar"
 export default function HomePage() {
 
   return (
-    <div>
-      <h1>Home Page</h1>'
-      <div>
+    <div style={styles.grid}>
+      <div style={styles.row1}>
         <SearchBar />
       </div>
     </div>
   )
+}
+
+const styles = {
+  grid: {
+    margin: '0 auto 0',
+    width: '95vw',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(1,fr)'
+  },
+  banner: {
+    gridRow: '1',
+  },
 }
 
 
