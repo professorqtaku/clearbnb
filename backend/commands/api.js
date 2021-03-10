de31b8e596a404089d9692537e5cc21b0cfb5d19
Qiling <professorqtaku@gmail.com>
2021-03-10 15:32:45 +0100

    backend register with bug
    ignores conditions defined in models
    
    Co-Authored-By: lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>

--- backend/commands/api.js
+++ backend/commands/api.js
@@ -1,29 +1,49 @@
 const models = require('../models.js')
+const bcrypt = require('bcrypt')
 
 module.exports = (app) => {
   app.post('/api/users', async (req, res) => {
-    const User = models['users']
+    let User = models['users']
     if (req.session.user) {
       res.json({ error: 'Someone is already logged in' })
       return
     }
-    let user = new User(req.body)
 
-    let userInColl = await User.findOne({ email: user.email })
-    if (userInColl) {
+    const saltRounds = 10
+    let hashedPassword = ''
+    try {
+      hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+    } catch (e) {
+      res.send('Password is missing')
+    }
+    let user = new User({ ...req.body, password: hashedPassword })
+
+    let userExist = await User.findOne({ email: user.email })
+    if (userExist) {
       res.send('E-mail already used/is missing')
       return
     }
     try {
-      user.save()
+      await user.save()
+      res.json({ success: true })
     }
     catch (e) {
       res.send('Save failed')
       return
     }
-
-    //req.session("user", user)
-    res.send(user)
   })
 
+}
+
+const hashPassword = async (password) => {
+  const saltRounds = 10;
+  const myPlaintextPassword = "s0//P4$$w0rD";
+  const someOtherPlaintextPassword = "not_bacon";
+  let hashed = "";
+  await bcrypt.genSalt(saltRounds, async (err, salt) => {
+    await bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
+      console.log("hash", hash);
+      hashed = hash;
+    });
+  });
 }
