90ef5aa32a668ec6444733ebf27b56e23e10282f
lukasbloom93 <lukas.bloom@hotmail.se>
2021-03-11 09:14:26 +0100

    registerpage

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,30 @@
 export default function RegisterPage() {
   return (
     <div>
-      <h1>Register Page</h1>
+      <h1>Register</h1>
+      <div className="container">
+        <div className="mb-3">
+        <label for="firstName" className="form-label">First Name</label>
+        <input type="firstName" className="form-control" id="firstNameInput" placeholder="First Name"></input>
+        </div>
+        <div className="mb-3">
+        <label for="lastName" className="form-label">Last Name</label>
+        <input type="lastName" className="form-control" id="lastNameInput" placeholder="Last Name"></input>
+        </div>
+        <div className="mb-3">
+        <label for="email" className="form-label">Email Address</label>
+        <input type="email" className="form-control" id="emailInput" placeholder="Email Address"></input>
+        </div>
+        <div className="mb-3">
+        <label for="password" className="form-label">Password</label>
+        <input type="password" className="form-control" id="passwordInput" placeholder="Password"></input>
+        </div>
+        <div className="mb-3">
+        <label for="password" className="form-label">Confirm Password</label>
+        <input type="password" className="form-control" id="confirmPasswordInput" placeholder="Confirm Password"></input>
+        </div>
+        <input className="btn btn-primary" type="submit" value="Submit"></input>
+      </div>    
     </div>
   );
 }
