2240b4b332f4758c785f521e85a8a2acffef007a
EdvinArnryd <edvin.arn@hotmail.com>
2021-03-12 14:53:08 +0100

    Fixed css properly
    centered all buttons
    
    Co-Authored-By: Qiling <72065263+professorqtaku@users.noreply.github.com>
    Co-Authored-By: snabelA2000 <72018736+snabelA2000@users.noreply.github.com>
    Co-Authored-By: lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,8 +1,8 @@
 import Radium from 'radium'
 export default function MyPage() {
   return (
-    <div className="container">
-      <div className="d-grid gap-2 text-center" style={styles.wrapper}>
+    <div className="container text-center">
+      <div className="d-grid gap-2" style={styles.wrapper}>
         <h1>My Page</h1>
         <button className="btn btn-primary" type="button" style={styles.btn}>My bookings</button>
         <button className="btn btn-primary" type="button" style={styles.btn}>My hostings</button>
@@ -17,15 +17,18 @@ const styles = {
   btn: {
     backgroundColor: 'var(--pink)',
     border: 'none',
-    marginBottom: '20px',
     paddingTop: '20px',
     paddingBottom: '20px',
-    fontSize: '2em',
-    marginLeft: 'min(200px, 20%)',
-    marginRight: 'min(200px, 20%)',
-    minWidth: '280px',
+    //fontSize: '2em',
+    //marginLeft: 'min(200px, 20%)',
+    //marginRight: 'min(200px, 20%)',
+    width: '100%',
     borderRadius: '50px',
     boxShadow: '2px 3px 5px grey',
+  },
+  wrapper: {
+    width: '100%',
+    maxWidth: '400px',
+    margin: '0 auto'
   }
-
 }
