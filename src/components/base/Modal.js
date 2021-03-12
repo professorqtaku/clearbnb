b51a78bd8b36c0bfd074e1cda7bc8f8008a4df32
Qiling <professorqtaku@gmail.com>
2021-03-12 13:29:30 +0100

    hotfix

--- src/components/base/Modal.js
+++ src/components/base/Modal.js
@@ -4,32 +4,32 @@ export default function LoginModal(props) {
   const modalId = props.modalId
   return (
     <div
-      class="modal fade"
+      className="modal fade"
       id={modalId}
       tabindex="-1"
       aria-labelledby="loginModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="loginModalLabel">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="loginModalLabel">
               {title}
             </h5>
             <button
               type="button"
-              class="btn-close"
+              className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
           </div>
-          <div class="modal-body">
+          <div className="modal-body">
             {content}
           </div>
-          <div class="modal-footer">
+          <div className="modal-footer">
             <button
               type="button"
-              class="btn btn-secondary"
+              className="btn btn-secondary"
               data-bs-dismiss="modal"
             >
               Close
