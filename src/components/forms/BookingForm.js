48158a58e9b3be9f2ff2b7f13fe85064a4850ec8
Qiling <professorqtaku@gmail.com>
2021-03-15 09:51:05 +0100

    added reactstrap

--- src/components/forms/BookingForm.js
+++ src/components/forms/BookingForm.js
@@ -1,46 +1,61 @@
-import React, { useState } from "react";
+import{ useState, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Form, FormGroup, Label } from 'reactstrap'
+import { UserContext } from '../../contexts/UserContextProvider'
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 export default function BookingForm() {
   const history = useHistory();
+  const { user } = useContext(UserContext)
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [guests, setGuests] = useState("");
 
-  return (
-    <div style={styles.gridContainer}>
-      <div style={styles.gridItem}>
-        From
-        <DatePicker
-          style={styles.datePicker}
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-        />
-      </div>
-      <div style={styles.gridItem}>
-        To
-        <DatePicker
-          style={styles.datePicker}
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-        />
-      </div>
-      <div style={styles.gridItem}>
-        <input
-          style={styles.input}
-          onChange={(event) => setGuests(event.target.value)}
-          placeholder="Guests"
-        />
-      </div>
-      <div style={styles.gridItem}>
-        <button className="btn" style={styles.input}>
-          Search
+  const bookButton = () => {
+    if (user) {
+      return (
+        <button className="btn" type="submit" onClick="book">Book</button>
+      )
+    }
+      return (
+        <button className="btn bg-light" type="submit" onClick="login">
+          Login
         </button>
-      </div>
+      );
+  }
+
+  return (
+    <div className="container" style={styles.container}>
+      <form className="row">
+        <div className="form-group col-6">
+          <Label for="date">From</Label>
+          <DatePicker
+            style={styles.datePicker}
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+        </div>
+        <div className="form-group col-6">
+          <Label for="date">To</Label>
+          <DatePicker
+            style={styles.datePicker}
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+          />
+        </div>
+        <div className="form-group col-6">
+          <input
+            style={styles.input}
+            onChange={(event) => setGuests(event.target.value)}
+            placeholder="Guests"
+          />
+        </div>
+        <div className="col-6">{bookButton()}</div>
+        <FormGroup></FormGroup>
+      </form>
     </div>
   );
 }
@@ -48,7 +63,6 @@ export default function BookingForm() {
 const styles = {
   gridContainer: {
     marginTop: "50px",
-    // backgroundColor: 'rgba(255, 255, 255, 0.73)',
     display: "grid",
     gridTemplateRows: "3fr",
     gridTemplateColumns: "repeat(2, 1fr)",
@@ -61,4 +75,7 @@ const styles = {
   datePicker: {
     width: "100%",
   },
+  container: {
+    backgroundColor: 'var(--lightgrey)'
+  }
 };
