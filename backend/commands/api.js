28a5c6bac2b6fa3423df5a2572f2b20a840d0c3d
lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>
2021-03-16 14:57:23 +0100

    added availablity
    Co-Authored-By: EdvinArnryd <72066252+EdvinArnryd@users.noreply.github.com>
    Co-Authored-By: Qiling <72065263+professorqtaku@users.noreply.github.com>
    Co-Authored-By: snabelA2000 <72018736+snabelA2000@users.noreply.github.com>
    Co-Authored-By: HananAlAni <72067261+HananAlAni@users.noreply.github.com>

--- backend/commands/api.js
+++ backend/commands/api.js
@@ -3,15 +3,26 @@ const models = require('../models')
 module.exports = (app) => {
   app.post('/api/bookings', async (req, res) => {
     const Booking = models['bookings']
+    const Availability = models['availabilities']
     let booking = new Booking(req.body)
 
+
+
     
-    if (req.body.hosting.guestAmount >= booking.guestAmount) {
-      await booking.save()
+    if (req.body.hosting.guestAmount < booking.guestAmount) {
+      res.json({error: "invalid input"})
+      return
+    }
+    
+     let availabilities = await Availability.find({ hosting: (booking.hosting)}).exec();
+     if(availabilities.length === 0){
+     res.json({error: "hosting unavailable"})
+     return
+     }
+    
+
+    await booking.save()
         .then(() => res.json(booking))
         .catch(() => res.json({ error: "Save failed" }))
-    } else {
-      res.json({error: "invalid input"})
-    }
   })
 }
