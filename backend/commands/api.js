69da31ba1d299b558d48632a3c180b72fec2c0dd
snabelA00 <ja000411@gmail.com>
2021-03-16 12:11:18 +0100

    validering guest amount

--- backend/commands/api.js
+++ backend/commands/api.js
@@ -4,14 +4,14 @@ module.exports = (app) => {
   app.post('/api/bookings', async (req, res) => {
     const Booking = models['bookings']
     let booking = new Booking(req.body)
-    try {
-      booking.save()
 
-    } catch (error) {
-      res.send('saving failed :(')
-      return
+    
+    if (req.body.hosting.guestAmount >= booking.guestAmount) {
+      await booking.save()
+        .then(() => res.json(booking))
+        .catch(() => res.json({ error: "Save failed" }))
+    } else {
+      res.json({error: "invalid input"})
     }
-
-    res.json(booking)
   })
 }
