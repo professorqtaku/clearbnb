d54b1886a8628e7d0632739861cf808f2e5eeb89
Qiling <professorqtaku@gmail.com>
2021-03-16 15:38:40 +0100

    added timePeriod at least 24hr
    Co-Authored-By: EdvinArnryd <72066252+EdvinArnryd@users.noreply.github.com>
    Co-Authored-By: lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>

--- backend/commands/api.js
+++ backend/commands/api.js
@@ -5,24 +5,53 @@ module.exports = (app) => {
     const Booking = models['bookings']
     const Availability = models['availabilities']
     let booking = new Booking(req.body)
-
+    
+    if (
+      req.body.hosting.guestAmount < booking.guestAmount ||
+      booking.timePeriod[1] - booking.timePeriod[0] < 86400000
+    ) {
+      res.json({ error: "invalid input" });
+      return;
+    }
 
 
     
-    if (req.body.hosting.guestAmount < booking.guestAmount) {
-      res.json({error: "invalid input"})
+    let availabilities = await Availability.find({ hosting: (booking.hosting) }).exec();
+    
+    let isValid = checkAvailability(availabilities, booking.timePeriod)
+    
+    if (!isValid) {
+      res.json({ error: "Booked time unavailable" });
       return
     }
-    
-     let availabilities = await Availability.find({ hosting: (booking.hosting)}).exec();
-     if(availabilities.length === 0){
-     res.json({error: "hosting unavailable"})
-     return
-     }
+
     
 
-    await booking.save()
+      await booking.save()
         .then(() => res.json(booking))
-        .catch(() => res.json({ error: "Save failed" }))
+        .catch(() => res.json({ error: "Save failed" }));
   })
+
+
+}
+
+const checkAvailability = (availabilities, periodToCheck) => {
+  if (availabilities.length === 0) {
+    return false
+  }
+
+  let isValid = false;
+  for (let a of availabilities) {
+    let startDate = a.timePeriod[0];
+    let endDate = a.timePeriod[1];
+    if (
+      periodToCheck[0] >= startDate &&
+      periodToCheck[1] <= endDate
+    ) {
+      isValid = true;
+      break;
+    }
+  }
+
+  return isValid
 }
