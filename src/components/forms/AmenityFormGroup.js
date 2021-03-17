fa1b1d890ae7dd0cf289206a5819b8bab1ad2c59
Qiling <professorqtaku@gmail.com>
2021-03-17 19:09:33 +0100

    added amenity checkboxes

--- src/components/forms/AmenityFormGroup.js
+++ src/components/forms/AmenityFormGroup.js
@@ -1,18 +1,55 @@
-import { Label, Col, FormGroup } from "reactstrap";
 import { useContext } from "react";
+import Radium from 'radium'
 import { AmenityContext } from "../../contexts/AmenityContextProvider";
 
-
-export default function AmenityFormGroup() {
+function AmenityFormGroup() {
   const { amenities } = useContext(AmenityContext);
 
   const amenityCheckbox = (amenity) => {
-    
-  }
+    return (
+      <div className="form-check" key={amenity.description}>
+        <input
+          className="form-check-input"
+          type="checkbox"
+          value=""
+          id="flexCheckDefault"
+        />
+        <label className="form-check-label">
+          <span className="material-icons align-self-end">
+            {amenity.icon ? amenity.icon : null}
+          </span>
+          {amenity.description}
+        </label>
+      </div>
+    );
+  };
 
   return (
-    <div>
-      {amenities ?  amenities.map(amenity => <p>{amenity.description}</p>): null}
+    <div className="container">
+      <h4 className="mb-3">Choose amenities</h4>
+      <div style={styles.container}>
+        {amenities ? amenities.map((amenity) => amenityCheckbox(amenity)) : null}
+      </div>
     </div>
   );
 }
+
+export default Radium(AmenityFormGroup);
+
+const styles = {
+  container: {
+    display: "grid",
+    gridTemplateColumns: "repeat(1, 1fr)",
+    gridGap: "2vw",
+    "@media (min-width: 400px)": {
+      gridTemplateColumns: "repeat(2, 1fr)",
+    },
+    "@media (min-width: 600px)": {
+      gridTemplateColumns: "repeat(3, 1fr)",
+    },
+    "@media (min-width: 800px)": {
+      gridTemplateColumns: "repeat(4, 1fr)",
+    },
+    
+  },
+};
