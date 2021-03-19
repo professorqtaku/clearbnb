22ce86b0a1a78933fbf8a70861ac64dce46eba6d
snabelA00 <ja000411@gmail.com>
2021-03-19 09:34:58 +0100

    hmm

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,19 @@
 import CityList from '../components/CityList'
+import { useState } from "react"
+import ResultList from "../components/lists/ResultList"
 
 export default function HomePage() {
 
+  const [isSearch, setIsSearch] = useState(false)
+
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <CityList />
+
+    <div className="homePage" style={styles.grid}>
+      <div>
+        <SearchBar setIsSearch={setIsSearch} />
+      </div>
+      {isSearch ? <ResultList /> : <CityList />}
     </div>
-  );
+  )
 }
