2a8815c250f39cd435b0030ab0cc398c92389820
snabelA00 <ja000411@gmail.com>
2021-03-23 09:03:17 +0100

    css searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,11 @@
+import Radium from "radium";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-export default function SearchBar(props) {
+function SearchBar(props) {
 
   const setIsSearch = props.setIsSearch
 
@@ -32,27 +33,27 @@ export default function SearchBar(props) {
       <div className="container" style={styles.container}>
         <div className="row">
 
-          <div className="col-md-3 col-lg-4">
+          <div className=" col-lg-4">
             <label>Location</label>
             <input className="form-control" type="text" placeholder="location" onChange={event => setLocation(event.target.value)} required />
           </div>
 
-          <div className="col-6 col-md-3 col-lg-2">
+          <div className="col-6 col-lg-2">
             <label>From</label>
             <DatePicker className="form-control" selected={startDate} onChange={date => setStartDate(date)} />
           </div>
 
-          <div className="col-6 col-md-3 col-lg-2">
+          <div className="col-6 col-lg-2">
             <label>To</label>
             <DatePicker className="form-control" selected={endDate} onChange={date => setEndDate(date)} />
           </div>
 
-          <div className="col-6 col-md-3 col-lg-2">
+          <div className="col-6 col-lg-2">
             <label>Guests</label>
             <input className="form-control" type="text" placeholder="Guests" onChange={event => setGuests(event.target.value)} />
           </div>
 
-          <div className="col-6 col-md-3 col-lg-2">
+          <div className="col-6 col-lg-2">
             <label></label>
             <button style={styles.button }className="form-control" type="submit">Search</button>
           </div>
@@ -63,11 +64,16 @@ export default function SearchBar(props) {
   )
 }
 
+export default Radium(SearchBar)
+
 const styles = {
   container: {
     padding: "1em",
     borderRadius: "10px",
     backgroundColor: "rgba(255,255,255,0.7)",
+    '@media (min-width: 700px) AND (max-width: 990px)': {
+      maxWidth: "500px"
+    },
   },
   button: {
     color: "white",
