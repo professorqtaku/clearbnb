38700dd3342a708b8f41c17d338d4021e7c5b1f4
EdvinArnryd <edvin.arn@hotmail.com>
2021-03-24 12:12:01 +0100

    404 is now centered
    kind of centered

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,19 +1,37 @@
 export default function NotFoundPage() {
   return (
-    <h1 style={styles}>Page not found:
-      <p style={styles.link}>{window.location.pathname}</p>
-    </h1>
+    <div style={styles}>
+      <h1 style={styles.title}>404
+      {/*<p style={styles.link}>{window.location.pathname}</p>*/}
+      </h1>
+      <p style={styles.link}>Page not found</p>
+    </div>
   )
 }
 
 const styles = {
-  width: '100%',
-  margin: 'auto',
+  /*width: '300px',
+  height: '400px',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  textAlign: 'center',*/
+
+  margin: '0 auto',
   textAlign: 'center',
-  marginTop: '20px',
-  marginBottom: '20px',
+  marginTop: '10%',
+  marginBottom: '20%',
+
+
+
+
+  title: {
+    fontSize: '10em',
+  },
 
   link: {
     color: 'grey',
+    fontSize: '2em',
   }
 }
