c5b77a99189030e8595300cf5db85cdcb994475b
EdvinArnryd <edvin.arn@hotmail.com>
2021-03-25 23:46:38 +0100

    Bootstrap grid css
    added new classes to make the page into columns that respond to the size of the page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,17 +1,20 @@
 import { getSuggestedQuery } from '@testing-library/dom'
-import robotError from '../assets/img/robotError.jpg'
+import robotError from '../assets/img/clearRob.png'
 
 export default function NotFoundPage() {
   return (
     <div style={styles}>
-      <div style={styles.text}>
-        <h1 style={styles.title}>404 <p style={styles.link}>Page not found</p></h1>
+      <div class="container-fluid">
+        <div class="row" style={styles.rows}>
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4" style={styles.text}>
+            <h1 style={styles.title}>404 <p style={styles.link}>Page not found</p></h1>
 
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8" style={styles.picture}>
+            <img style={styles.image} src={robotError} />
+          </div>
+        </div>
       </div>
-      <div style={styles.picture}>
-        <img style={styles.image} src={robotError} />
-      </div>
-
     </div>
   )
 }
@@ -24,11 +27,18 @@ const styles = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   textAlign: 'center',*/
-  display: 'grid',
-  gridTemplateColumns: '40% 60%',
-  margin: '0 auto',
+
   marginTop: '5%',
-  marginBottom: '3%',
+  marginBottom: '2%',
+
+  maxWidth: '1200px',
+  margin: '0 auto',
+
+
+  rows: {
+    marginTop: '20px',
+  },
+
   text: {
     textAlign: 'right',
   },
@@ -43,7 +53,4 @@ const styles = {
     color: 'grey',
     fontSize: '39px',
   },
-  image: {
-    backgroundColor: 'blue',
-  }
 }
