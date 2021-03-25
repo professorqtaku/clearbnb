a5a3b891bbc627769ad25ca3a49a511d51919f39
HananAlAni <72067261+HananAlAni@users.noreply.github.com>
2021-03-25 10:25:43 +0100

    ny footer som passar bättre med websidan

--- src/components/base/Footer.js
+++ src/components/base/Footer.js
@@ -1,56 +1,53 @@
-import React from "react";
+//import Radium from 'radium'
+import { useHistory } from "react-router-dom";
+import {Container,Row,Col} from "reactstrap"
+import React from 'react'
 
-import { Link } from 'react-router-dom'
-import { Container, Row, Col } from "reactstrap";
+
+
 
 const Footer = () => {
+
+ const history=useHistory();
+ const goTo = (e) => {
+  history.push("/" + e.target.value);
+  }
   return (
-    <Container fluid className="bg-light border-top border-info mt-5">
-      <Container
-        className="mt-5 mb-3"      
-      >
-        <Row style={{color:"black"}}className="font-small indigo " >
-          <Col sm="12" md="3" className="mb-3">
-            <h6  className="text-uppercase font-weight-bold text-center">
-              <Link to="/citylist">Housing</Link>
-            </h6>
+    <div className="container-fluid">
+        <Row style={{color:"var(--pink)"}}className="font-small indigo " >
+
+          <Col sm="5" md="3" className="mb-3">
           </Col>
-          <Col sm="12" md="3" className="mb-3">
-            <h6 className="text-uppercase font-weight-bold text-center">
-              <Link to="/about">About us</Link>
-            </h6>
+          <Col sm="5" md="3" className="mb-3">
+          <button className="mt-5 mb-3" style={styles.button} onClick={goTo} value="about">
+           About us
+          </button>
           </Col>
-          
-          <Col sm="12" md="3" className="mb-3">
-            <h6 className="text-uppercase font-weight-bold text-center text-info">
-              <Link to="/contact">contact</Link>
-            </h6>
+
+          <Col sm="5" md="3" className="mb-3">
+          <button className="mt-5 mb-3" style={styles.button} onClick={goTo} value="contact">
+           Contact us
+          </button>
           </Col>
-          <Col sm="12" md="3" className="mb-3">
-            <h6 className="text-uppercase font-weight-bold text-center text-info">
-              <Link to="/register">sign up</Link>
-            </h6>
-          </Col>
-        </Row>
-        <hr className="rgba-white-light" />
-        <Row>
-          <Col
-            md="12"
-            className="my-3 d-flex text-center justify-content-center mb-md-0 mb-4"
-          >
-             
-          </Col>
-        </Row>
-        <hr
-          className="clearfix d-md-none rgba-white-light"
-          style={{ margin: "10% 15% 5%" }}
-        />
-      </Container>
-      <Row className="text-center">
-          <Col md="12">©Copyright 2021 | Group 5 TH Lund</Col>
-      </Row>
-    </Container>
-  );
-};
+          </Row>
+   </div>
+
+   );
+
+  }
 
 export default Footer;
+
+const styles = {
+button: {
+backgroundColor: 'var(--pink)',
+ color: 'white',
+  fontWeight: 'bold',
+  width: '100%',
+  borderRadius: '50px',
+  ':focus': {
+    border: 'none !important'
+  }
+}
+}
+   
