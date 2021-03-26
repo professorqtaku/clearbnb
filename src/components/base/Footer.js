50bcee6e704953091555b1772cac42ebefc9ee82
Qiling <professorqtaku@gmail.com>
2021-03-26 09:46:31 +0100

    Revert "Update Footer.js"
    This reverts commit 235ec5e144bd1e0a6ad8f846ed9a7ea1d87db48c.

--- src/components/base/Footer.js
+++ src/components/base/Footer.js
@@ -1,56 +1,64 @@
 import React from "react";
-
-import { Link } from 'react-router-dom'
-import { Container, Row, Col } from "reactstrap";
+import {useHistory } from "react-router-dom"
+import Radium from 'radium'
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react-dom";
 
 const Footer = () => {
+  const history = useHistory()
+  const goTo = (event) => {
+    console.log(event.target);
+    console.log(event.target.value);
+    history.push(`/${event.target.value}`)
+  }
+
   return (
-    <Container fluid className="bg-light border-top border-info mt-5">
-      <Container
-        className="mt-5 mb-3"      
-      >
-        <Row style={{color:"black"}}className="font-small indigo " >
-          <Col sm="12" md="3" className="mb-3">
-            <h6  className="text-uppercase font-weight-bold text-center">
-              <Link to="/citylist">Housing</Link>
-            </h6>
-          </Col>
-          <Col sm="12" md="3" className="mb-3">
-            <h6 className="text-uppercase font-weight-bold text-center">
-              <Link to="/about">About us</Link>
-            </h6>
-          </Col>
-          
-          <Col sm="12" md="3" className="mb-3">
-            <h6 className="text-uppercase font-weight-bold text-center text-info">
-              <Link to="/contact">contact</Link>
-            </h6>
-          </Col>
-          <Col sm="12" md="3" className="mb-3">
-            <h6 className="text-uppercase font-weight-bold text-center text-info">
-              <Link to="/register">sign up</Link>
-            </h6>
-          </Col>
-        </Row>
-        <hr className="rgba-white-light" />
-        <Row>
-          <Col
-            md="12"
-            className="my-3 d-flex text-center justify-content-center mb-md-0 mb-4"
-          >
-             
-          </Col>
-        </Row>
-        <hr
-          className="clearfix d-md-none rgba-white-light"
-          style={{ margin: "10% 15% 5%" }}
-        />
-      </Container>
-      <Row className="text-center">
-          <Col md="12">©Copyright 2021 | Group 5 TH Lund</Col>
-      </Row>
-    </Container>
+    <div className="container-fluid" style={styles.container}>
+      <div className="container mt-5">
+        <div className="row font-small">
+          <div className="col-md-4 mb-3 text-center" style={styles.link}>
+            <button className="btn" onClick={goTo} value="about">
+              <h6
+                className="text-uppercase font-weight-bold text-muted"
+              >
+                About us
+              </h6>
+            </button>
+          </div>
+          <div className="col-md-4 mb-3 text-center" style={styles.link}>
+            <button className="btn" onClick={goTo} value="contact">
+              <h6
+                className="text-uppercase font-weight-bold text-muted"
+              >
+                Contact us
+              </h6>
+            </button>
+          </div>
+          <div className="col-md-4 mb-3 text-center" style={styles.link}>
+            <button className="btn" onClick={goTo} value="register">
+              <h6
+                className="text-uppercase font-weight-bold text-muted"
+              >
+                Sign up
+              </h6>
+            </button>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className="row text-center">
+        <h6 className="text-muted">&copy;Copyright 2021 | Group 5 TH Lund</h6>
+      </div>
+    </div>
   );
 };
 
-export default Footer;
+export default Radium(Footer);
+
+const styles = {
+  container: {
+    backgroundColor: "var(--lightgrey)",
+    borderTop: "2px solid var(--darkgrey)",
+  },
+  link: {
+  },
+};
