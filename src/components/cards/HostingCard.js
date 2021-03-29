87abb776a4ec6101d88138a71b56972e715f5294
snabelA00 <ja000411@gmail.com>
2021-03-29 09:04:09 +0200

    lorem ipsum

--- src/components/cards/HostingCard.js
+++ src/components/cards/HostingCard.js
@@ -27,6 +27,7 @@ const HostingCard = (props) => {
 
         <div className="card-body col-12 col-md-7 col-lg-7 ">
           <div className="row">
+            <p className="col-12 " style={styles.font}>{hosting.address.city}</p>
             <h5 className="col-12 card-title">{hosting.title}</h5>
             <p style={styles.font} className="col-12 card-text">{hosting.guestAmount} guests</p>
             <p style={styles.font} className="col-6 card-text">{hosting.bedAmount} beds</p>
