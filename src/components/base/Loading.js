bef2a97677640de25e3e1e64dca7936b28160f66
Qiling <professorqtaku@gmail.com>
2021-03-29 11:29:20 +0200

    added style

--- src/components/base/Loading.js
+++ src/components/base/Loading.js
@@ -2,8 +2,11 @@ export default function Loading() {
 
   return (
     <div className="container-fluid" style={styles.container}>
-      <div className="d-flex justify-content-center align-items-center" style={styles.container}>
-      <strong className="m-3">Loading...</strong>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={styles.container}
+      >
+        <p className="m-3" style={styles.text}>Loading...</p>
         <div className="spinner-border" role="status">
           <span className="sr-only"></span>
         </div>
@@ -18,5 +21,8 @@ const styles = {
     backgroundColor: "var(--lightgrey)",
     borderRadius: "10px",
     color: "var(--darkgrey)"
+  },
+  text: {
+    fontWeight: "bold"
   }
 }
