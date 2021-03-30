86173b93d858cc5300329192aed02bef0d7b1196
snabelA00 <ja000411@gmail.com>
2021-03-30 14:35:57 +0200

    style booking confirmation view

--- src/components/views/BookingConfirmationView.js
+++ src/components/views/BookingConfirmationView.js
@@ -1,6 +1,6 @@
 import Radium from 'radium'
 import { UserContext } from '../../contexts/UserContextProvider'
-import { useContext, useState, useEffect} from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { HostingContext } from '../../contexts/HostingContextProvider'
 import { useHistory } from 'react-router-dom'
 import LoadingScreen from "../base/Loading"
@@ -8,36 +8,62 @@ import LoadingScreen from "../base/Loading"
 function BookingConfirmationView(props) {
   const { booking } = props
   const { hostings, fetchHostings } = useContext(HostingContext)
-  const [hosting, setHosting ] = useState(null)
-  
+  const [hosting, setHosting] = useState(null)
+
   useEffect(() => {
     fetchHostings()
     let matchedHosting = hostings.filter((hosting) => booking.hosting._id === hosting._id)[0]
     console.log(matchedHosting)
+    console.log(booking)
     setHosting(matchedHosting)
   }, [])
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const showDate = () => {
+    let startDate = new Date(booking.timePeriod[0])
+    let endDate = new Date(booking.timePeriod[1])
+    startDate = startDate.getDate() + ' ' + capitalizeFirstLetter(startDate.toLocaleString("default", { month: "long" }))
+    endDate = endDate.getDate() + ' ' + capitalizeFirstLetter(endDate.toLocaleString("default", { month: "long" }))
+
+    return (
+      startDate + ' - ' + endDate
+    );
+  }
 
 
 
   const renderView = (hosting, booking) => {
     return (
-    <div style={styles.gridContainer} className="container">
-      <p>{booking.hosting.title}</p>
-      <p>address: {hosting.address.city}</p>
-      <p>host: {booking.hosting.host}</p>
-      <p>totalPrice: {booking.totalPrice}</p>
-      <p>guestAmount: {booking.guestAmount}</p>
-      <p>From: {booking.timePeriod[0]}</p>
-      <p>To: {booking.timePeriod[1]}</p>
-
+      <div className="container" style={styles.gridContainer}>
+        <div className="row">
+          <div className="col">
+            <p>{booking.hosting.title}</p>
+            <div className="col">
+              <p>
+                <span>{showDate()}</span>
+                <span>, {hosting.address.street}, {hosting.address.city}</span>
+              </p>
+            </div>
+          </div>
+          <div className="col">
+            <p>Contact host:</p>
+            <p>{hosting.host.firstName} {hosting.host.lastName}</p>
+            <p>{hosting.host.email}</p>
+            <br></br>
+            <p>totalPrice: {booking.totalPrice}</p>
+          </div>
+        </div>
       </div>
     )
-    
+
   }
 
   return (
     <div className="container">
-      {hosting ? renderView(hosting, booking) : <LoadingScreen/>}
+      {hosting ? renderView(hosting, booking) : <LoadingScreen />}
     </div>
   );
 
